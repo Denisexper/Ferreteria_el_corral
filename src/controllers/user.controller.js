3d25125025409822ef42c2a513327c7d08649573
Denisexper <clasic.exper2023@gmail.com>
2024-12-30 22:09:02 -0600

    feat: finalizating users controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,18 +6,29 @@ class UserController {
         try {
             const {name, email, password, role} = req.body
             const newuser = await User.create({ name, email, password, role })
+
+            if(password.lenght < 8){
+                return res.send({
+                    status: false,
+                    message: "Password must be at least 8 characters long"
+                })
+            }
             res.status(201).send({
                 message: "User created successfully",
                 newuser
             })
+
         } catch (error) {
-            console.log(error)
+            res.send({
+                status: false,
+                message: error.message
+            })
         }
     }
 
     async getAllUsers (req, res) {
         try {
-            const users = await User.find()
+            const users = await User.find().select("-__v")
             res.status(200).send({
                 message: "Users retrieved successfully",
                 users
@@ -25,6 +36,66 @@ class UserController {
         } catch (error) {
             res.status(400).send({
                 message: "Error retrieving users",
+                error
+            })
+        }
+    }
+
+    async getUser (req, res) {
+        try {
+            const { id } = req.params
+            const user = await User.findById(id).select("-__v")
+            if (!user) {
+                res.status(404).send({
+                    message: "please enter a correct id"
+                })
+            }
+            res.status(200).send({
+                message: "User retrieved successfully",
+                user
+            })
+        } catch (error) {
+            res.status(400).send({
+                message: "Error retrieving user",
+                error
+            })
+        }
+    }
+
+    async updateUser (req, res) {
+        try {
+            const { id } = req.params;
+            const { name, email, password, role } = req.body;
+            const user = await User.findByIdAndUpdate(id, {name, email, password, role}, {new: true})
+            
+            if(password.lenght < 8){
+                res.status(400).send({
+                    message: "Password must be at least 8 characters long",
+                })
+            }
+            res.status(200).send({
+                message: "User updated successfully",
+                user
+            })
+        } catch (error) {
+            res.status(400).send({
+                message: "Error updating user",
+                error
+            })
+        }
+    }
+
+    async deleteUser (req, res) {
+        try {
+            const { id } = req.params;
+            const user = await User.findByIdAndDelete(id)
+            res.status(200).send({
+                message: "User deleted successfully",
+                user
+            })
+        } catch (error) {
+            res.status(400).send({
+                message: "Error deleting user",
                 error
             })
         }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,5 +7,7 @@ const app = express.Router()
 
 app.post("/create-user", usercontroll.createUser)
 app.get("/get-users", usercontroll.getAllUsers)
-
+app.get("/get-user/:id", usercontroll.getUser)
+app.put("/update-user/:id", usercontroll.updateUser)
+app.delete("/delete-user/:id", usercontroll.deleteUser)
 export default app;
