1b126d94c3d36df85da3a711af52d103187ebf96
Denisexper <clasic.exper2023@gmail.com>
2025-01-02 19:49:35 -0600

    feat: change the port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`server is runing on port ${PORT}`)
